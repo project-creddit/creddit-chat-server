d2b0e8d9234f9fc6527b5232760a13506ad9643a
oereo <dlstpgns0406@gmail.com>
2022-04-03 15:16:22 +0900

    chore: project setting

---
+++ src/main/java/com/creddit/credditchatserver/CredditChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.creddit.credditchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredditChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredditChatServerApplication.class, args);
+    }
+
+}
