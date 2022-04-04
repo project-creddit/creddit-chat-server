4b279c1ec23a6955f8bbfbb01ac5ee83c12ea596
oereo <dlstpgns0406@gmail.com>
2022-04-04 22:47:27 +0900

    feat: TraceAspect 클래스 추가

---
+++ src/main/java/com/creddit/credditchatserver/trace/TraceAspect.java
@@ -0,0 +1,17 @@
+package com.creddit.credditchatserver.trace;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class TraceAspect {
+
+    @Before("@annotation(com.creddit.credditchatserver.trace.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
