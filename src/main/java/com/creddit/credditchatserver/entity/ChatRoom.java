595843b2ff0065de3ea9b6ab73a066d2a5a854c8
oereo <dlstpgns0406@gmail.com>
2022-04-05 14:57:06 +0900

    feat: ChatRoom 엔터디 추가

--- src/main/java/com/creddit/credditchatserver/entity/ChatRoom.java
+++ src/main/java/com/creddit/credditchatserver/entity/ChatRoom.java
@@ -1,2 +1,22 @@
-package com.creddit.credditchatserver.entity;public class ChatRoom {
+package com.creddit.credditchatserver.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue
+    @Column(name="chat_room_id")
+    private Long id;
+
+    private String name;
+
+    private int userCount;
 }
