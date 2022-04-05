fff015f41d14200d3ad8fd76721b67d308736592
oereo <dlstpgns0406@gmail.com>
2022-04-05 15:00:19 +0900

    feat: ChatRoomRepository 추가

--- src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
+++ src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
@@ -1,2 +1,10 @@
-package com.creddit.credditchatserver.repository;public class ChatRoomRepository {
+package com.creddit.credditchatserver.repository;
+
+import com.creddit.credditchatserver.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 }
