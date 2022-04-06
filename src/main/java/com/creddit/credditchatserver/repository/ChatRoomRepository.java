5ac386c370e3c6dc7bfd74eba97367d1aa2fd3eb
oereo <dlstpgns0406@gmail.com>
2022-04-06 15:58:35 +0900

    feat: ChatRoomRepository에 findByUuid method 추가

--- src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
+++ src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    ChatRoom findByUuid(String uuid);
 }
