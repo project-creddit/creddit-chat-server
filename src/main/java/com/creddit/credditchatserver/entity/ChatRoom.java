57b92b407b23412308077faa3328829030d8a756
oereo <dlstpgns0406@gmail.com>
2022-04-06 15:54:51 +0900

    feat: ChatRoom entity 추가

--- src/main/java/com/creddit/credditchatserver/entity/ChatRoom.java
+++ src/main/java/com/creddit/credditchatserver/entity/ChatRoom.java
@@ -1,22 +1,41 @@
 package com.creddit.credditchatserver.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-@Data
+@Builder
 @Entity
+@Table(name = "chat_room")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ChatRoom {
 
     @Id
     @GeneratedValue
     @Column(name="chat_room_id")
     private Long id;
-
+    private String uuid;
     private String name;
-
     private int userCount;
+
+    @BatchSize(size = 1000)
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<UserChatRoom> userChatRooms = new ArrayList<>();
+
+    public static ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.uuid = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+
 }
