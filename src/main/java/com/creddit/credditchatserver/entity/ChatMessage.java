828684ae425819517ecf69f574d524af043ea7c0
oereo <dlstpgns0406@gmail.com>
2022-04-11 19:38:41 +0900

    feat: createChatMessage 생성자 메서드 추가

--- src/main/java/com/creddit/credditchatserver/entity/ChatMessage.java
+++ src/main/java/com/creddit/credditchatserver/entity/ChatMessage.java
@@ -1,7 +1,9 @@
 package com.creddit.credditchatserver.entity;
 
+import com.creddit.credditchatserver.dto.ChatMessageDto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 
@@ -11,6 +13,7 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Slf4j
 public class ChatMessage {
 
     @Id
@@ -28,6 +31,16 @@ public class ChatMessage {
     private String senderName;
 
     private ChatMessageType status;
+
+    public static ChatMessage createChatMessage(ChatMessageDto chatMessageDto, ChatRoom chatRoom) {
+        ChatMessage chatMessage = new ChatMessage();
+        log.info("chatMessageDto: {}", chatMessageDto);
+        chatMessage.setChatRoom(chatRoom);
+        chatMessage.setMessage(chatMessageDto.getMessage());
+        chatMessage.setSenderName(chatMessage.getSenderName());
+        chatMessage.setStatus(chatMessage.getStatus());
+        return chatMessage;
+    }
 
 }
 
