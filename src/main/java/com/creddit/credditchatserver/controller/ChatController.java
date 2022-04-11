2ca648dac8cbbd8304a71804b093ea005dfc5b40
oereo <dlstpgns0406@gmail.com>
2022-04-11 19:38:09 +0900

    refactor: 사용하지 않는 log용 코드 삭제

--- src/main/java/com/creddit/credditchatserver/controller/ChatController.java
+++ src/main/java/com/creddit/credditchatserver/controller/ChatController.java
@@ -1,18 +1,21 @@
 package com.creddit.credditchatserver.controller;
 
+import com.creddit.credditchatserver.dto.ChatMessageDto;
 import com.creddit.credditchatserver.entity.ChatMessage;
 import com.creddit.credditchatserver.service.ChatMessageService;
+import com.creddit.credditchatserver.service.ChatRoomService;
 import com.creddit.credditchatserver.trace.Trace;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
+@RestController
 public class ChatController {
 
     private SimpMessagingTemplate simpMessagingTemplate;
@@ -20,15 +23,14 @@ public class ChatController {
 
     @Trace
     @MessageMapping("/message")
-    @SendTo("/chatroom/public")
-    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        return chatMessage;
+    public ChatMessageDto register(@Payload ChatMessageDto chatMessageDto, SimpMessageHeaderAccessor headerAccessor) {
+        chatMessageService.createChatMessage(chatMessageDto);
+        return chatMessageDto;
     }
 
     @MessageMapping("/private-message")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         simpMessagingTemplate.convertAndSendToUser(chatMessage.getSenderName(),"/private",chatMessage);
-        log.info("chat {}", chatMessage);
         return chatMessage;
     }
 
