b74e3259c367207fbd3ba8f73adf0b0ee20863c4
oereo <dlstpgns0406@gmail.com>
2022-04-11 19:39:55 +0900

    feat: ChatMessageService 안의 createChatMessage 메서드 구현

--- src/main/java/com/creddit/credditchatserver/service/ChatMessageService.java
+++ src/main/java/com/creddit/credditchatserver/service/ChatMessageService.java
@@ -1,22 +1,29 @@
 package com.creddit.credditchatserver.service;
 
+import com.creddit.credditchatserver.dto.ChatMessageDto;
 import com.creddit.credditchatserver.entity.ChatMessage;
 import com.creddit.credditchatserver.entity.ChatRoom;
 import com.creddit.credditchatserver.repository.ChatMessageRepository;
 import com.creddit.credditchatserver.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     @Transactional
-    public Long createChatMessage(ChatMessage chatMessage) {
+    public Long createChatMessage(ChatMessageDto chatMessageDto) {
+        log.debug("chatMessageService: {}", chatMessageDto.getMessage());
+        ChatRoom chatRoom = chatRoomRepository.findByUuid(chatMessageDto.getChatRoomId());
+        ChatMessage chatMessage = ChatMessage.createChatMessage(chatMessageDto, chatRoom);
         chatMessageRepository.save(chatMessage);
         return chatMessage.getId();
     }
