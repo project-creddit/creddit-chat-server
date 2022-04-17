e3dc5deac41092a87823b63825d7670446a43133
oereo <dlstpgns0406@gmail.com>
2022-04-17 20:54:03 +0900

    refactor: configureMessageBroker 수정

--- src/main/java/com/creddit/credditchatserver/config/WebSocketConfig.java
+++ src/main/java/com/creddit/credditchatserver/config/WebSocketConfig.java
@@ -17,8 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chatroom", "/user");
-        registry.setUserDestinationPrefix("/user");
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
