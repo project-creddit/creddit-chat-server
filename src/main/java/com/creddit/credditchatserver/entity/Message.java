a310824f677d1585e207e0a146199303fbd608b2
oereo <dlstpgns0406@gmail.com>
2022-04-17 13:58:29 +0900

    refactor: MessageEntity 수정

--- src/main/java/com/creddit/credditchatserver/entity/Message.java
+++ src/main/java/com/creddit/credditchatserver/entity/Message.java
@@ -1,2 +1,18 @@
-package com.creddit.credditchatserver.entity;public class Message {
+package com.creddit.credditchatserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+
+    private String sender;
+    private String receiver;
+    private String message;
+
 }
