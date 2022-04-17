49e8b0e1971f9ec18368bea5d869ca9d1b82c807
oereo <dlstpgns0406@gmail.com>
2022-04-17 22:52:02 +0900

    feat: ALREADY_EXIST_USER 추가

--- src/main/java/com/creddit/credditchatserver/exception/user/UserExceptionType.java
+++ src/main/java/com/creddit/credditchatserver/exception/user/UserExceptionType.java
@@ -4,7 +4,8 @@ import com.creddit.credditchatserver.exception.BaseExceptionType;
 import org.springframework.http.HttpStatus;
 
 public enum UserExceptionType implements BaseExceptionType {
-    ALREADY_EXIST_CHAT_USER(HttpStatus.BAD_REQUEST, "이미 등록이 된 회원입니다"),
+    ALREADY_EXIST_CHAT_USER(HttpStatus.BAD_REQUEST, "이미 채팅유저로 등록이 된 회원입니다"),
+    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 가입된 유저입니다."),
     NOT_FOUNT_USER(HttpStatus.NOT_FOUND, "일치하는 회원 정보가 없습니다.");
 
     private HttpStatus status;
