ac8ef824421dcb0e3da38aa656a7d2894f5c1b41
oereo <dlstpgns0406@gmail.com>
2022-04-17 22:51:35 +0900

    feat: UserController 추가

--- src/main/java/com/creddit/credditchatserver/controller/UserController.java
+++ src/main/java/com/creddit/credditchatserver/controller/UserController.java
@@ -1,2 +1,28 @@
-package com.creddit.credditchatserver.controller;public class UserController {
+package com.creddit.credditchatserver.controller;
+
+import com.creddit.credditchatserver.entity.User;
+import com.creddit.credditchatserver.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+@AllArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+    @Deprecated
+    @PostMapping("/register")
+    public ResponseEntity<Boolean> register(@RequestBody User user) throws Exception{
+        userService.createUser(user);
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 }
