ae75a0258af329f3e17e62c6438ffe044d5b623c
oereo <dlstpgns0406@gmail.com>
2022-04-17 20:51:42 +0900

    feat: TraceAspect annotation 추가

--- src/main/java/com/creddit/credditchatserver/exception/ExceptionAdvice.java
+++ src/main/java/com/creddit/credditchatserver/exception/ExceptionAdvice.java
@@ -1,8 +1,10 @@
 package com.creddit.credditchatserver.exception;
 
+import com.creddit.credditchatserver.trace.TraceAspect;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
+@Import(TraceAspect.class)
 public class ExceptionAdvice {
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity handleBaseEx(BaseException exception) {
+    public ResponseEntity handleBaseException(BaseException exception) {
         log.error("BaseException errorMessage(): {}", exception.getExceptionType().getErrorMessage());
         log.error("BaseException errorMessage(): {}", exception.getExceptionType().getHttpStatus());
         return new ResponseEntity(new ExceptionDto(
@@ -21,12 +24,6 @@ public class ExceptionAdvice {
                 exception.getExceptionType().getHttpStatus(),
                 exception.getExceptionType().getErrorMessage()
         ), HttpStatus.OK);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity handleMemberEx(Exception exception) {
-        exception.printStackTrace();
-        return new ResponseEntity(HttpStatus.OK);
     }
 
     @Data
