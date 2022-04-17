0540cac573cffc5b492f35704d3f69550ae4d6e7
oereo <dlstpgns0406@gmail.com>
2022-04-17 20:50:17 +0900

    feat: UserException 관련 클래스 추가

--- src/main/java/com/creddit/credditchatserver/exception/user/UserException.java
+++ src/main/java/com/creddit/credditchatserver/exception/user/UserException.java
@@ -1,2 +1,17 @@
-package com.creddit.credditchatserver.exception.user;public class UserException {
+package com.creddit.credditchatserver.exception.user;
+
+import com.creddit.credditchatserver.exception.BaseException;
+import com.creddit.credditchatserver.exception.BaseExceptionType;
+
+public class UserException extends BaseException {
+    private final BaseExceptionType exceptionType;
+
+    public UserException(BaseExceptionType exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return exceptionType;
+    }
 }
--- src/main/java/com/creddit/credditchatserver/exception/user/UserExceptionType.java
+++ src/main/java/com/creddit/credditchatserver/exception/user/UserExceptionType.java
@@ -1,2 +1,28 @@
-package com.creddit.credditchatserver.exception.user;public interface UserExceptionType {
+package com.creddit.credditchatserver.exception.user;
+
+import com.creddit.credditchatserver.exception.BaseExceptionType;
+import org.springframework.http.HttpStatus;
+
+public enum UserExceptionType implements BaseExceptionType {
+    ALREADY_EXIST_CHAT_USER(HttpStatus.BAD_REQUEST, "이미 등록이 된 회원입니다"),
+    NOT_FOUNT_USER(HttpStatus.NOT_FOUND, "일치하는 회원 정보가 없습니다.");
+
+    private HttpStatus status;
+    private String errorMessage;
+
+    UserExceptionType(HttpStatus status, String errorMessage) {
+        this.status = status;
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
 }
