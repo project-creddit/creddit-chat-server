00f697d6499543e6999f8a27b1e608616ed0060b
oereo <dlstpgns0406@gmail.com>
2022-04-17 14:04:15 +0900

    feat: RedisConfig 추가

--- src/main/java/com/creddit/credditchatserver/config/RedisConfig.java
+++ src/main/java/com/creddit/credditchatserver/config/RedisConfig.java
@@ -1,2 +1,55 @@
-package com.creddit.credditchatserver.config;public class RedisConfig {
+package com.creddit.credditchatserver.config;
+
+import com.creddit.credditchatserver.entity.ChatRoom;
+import com.creddit.credditchatserver.entity.Message;
+import com.creddit.credditchatserver.entity.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, ChatRoom> chatRoomRedisTemplate() {
+        RedisTemplate<String, ChatRoom> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatRoom.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(ChatRoom.class));
+        return redisTemplate;
+    }
+
+    @Bean
+    @Deprecated
+    public RedisTemplate<String, User> userTemplate(){
+        RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Message> messageTemplate(){
+        RedisTemplate<String, Message> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Message.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Message.class));
+        return redisTemplate;
+    }
 }
