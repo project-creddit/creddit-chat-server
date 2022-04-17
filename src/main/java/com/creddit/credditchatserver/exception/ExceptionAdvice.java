e245ce2c488d45dc1906aad4d0a78802fb5fdf97
oereo <dlstpgns0406@gmail.com>
2022-04-17 20:50:39 +0900

    feat: ExceptionAdvice 추가

--- src/main/java/com/creddit/credditchatserver/exception/ExceptionAdvice.java
+++ src/main/java/com/creddit/credditchatserver/exception/ExceptionAdvice.java
@@ -1,2 +1,39 @@
-package com.creddit.credditchatserver.exception;public class ExceptionAdvice {
+package com.creddit.credditchatserver.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity handleBaseEx(BaseException exception) {
+        log.error("BaseException errorMessage(): {}", exception.getExceptionType().getErrorMessage());
+        log.error("BaseException errorMessage(): {}", exception.getExceptionType().getHttpStatus());
+        return new ResponseEntity(new ExceptionDto(
+                exception.getExceptionType().getHttpStatus().value(),
+                exception.getExceptionType().getHttpStatus(),
+                exception.getExceptionType().getErrorMessage()
+        ), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleMemberEx(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ExceptionDto {
+        private int status;
+        private HttpStatus statusMessage;
+        private String errorMessage;
+    }
 }
