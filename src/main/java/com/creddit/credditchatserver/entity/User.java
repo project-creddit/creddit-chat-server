4369eb3ff1e05683cd7029ceee383843b9f6d5d3
oereo <dlstpgns0406@gmail.com>
2022-04-17 13:59:00 +0900

    feat: chatting을 위한 임시 User 모델 추가

--- src/main/java/com/creddit/credditchatserver/entity/User.java
+++ src/main/java/com/creddit/credditchatserver/entity/User.java
@@ -1,2 +1,17 @@
-package com.creddit.credditchatserver.entity;public class User {
+package com.creddit.credditchatserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Deprecated
+public class User {
+    private String id;
+    private String name;
+    private String password;
 }
