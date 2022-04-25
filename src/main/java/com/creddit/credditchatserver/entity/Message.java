9cf73d5309f1b87804bdf92b5349215677b4edd2
oereo <dlstpgns0406@gmail.com>
2022-04-25 14:50:44 +0900

    feat: Message 엔터티에 createdDate 필드 추가

--- src/main/java/com/creddit/credditchatserver/entity/Message.java
+++ src/main/java/com/creddit/credditchatserver/entity/Message.java
@@ -5,14 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class Message {
+public class Message extends {
 
     private String sender;
     private String receiver;
     private String message;
+    private String createdDate;
 
 }
