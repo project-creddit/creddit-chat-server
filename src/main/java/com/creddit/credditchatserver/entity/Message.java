e9a0ca3dcfcf4e7f3417e47c4b776320ce8e8dde
oereo <dlstpgns0406@gmail.com>
2022-04-25 14:55:17 +0900

    refactor: 사용하지 않는 모듈 삭제

--- src/main/java/com/creddit/credditchatserver/entity/Message.java
+++ src/main/java/com/creddit/credditchatserver/entity/Message.java
@@ -1,11 +1,9 @@
 package com.creddit.credditchatserver.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
