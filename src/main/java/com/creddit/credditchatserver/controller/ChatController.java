1dd1fc4f15064d779bc14363b573087eb291cfc0
oereo <dlstpgns0406@gmail.com>
2022-05-09 15:21:25 +0900

    feat: 채팅방 하나에 대한 메시지 리스트 api 추가

--- src/main/java/com/creddit/credditchatserver/controller/ChatController.java
+++ src/main/java/com/creddit/credditchatserver/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.creddit.credditchatserver.entity.ChatRoom;
 import com.creddit.credditchatserver.service.ChatService;
 import com.creddit.credditchatserver.trace.TraceAspect;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @Import(TraceAspect.class)
@@ -32,5 +36,12 @@ public class ChatController {
     public ResponseEntity<Collection<ChatRoom>> fetchAllChatRooms(@PathVariable String userName){
         Collection<ChatRoom> chatRooms = chatService.getChatRooms(userName);
         return new ResponseEntity<>(chatRooms, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userName}/chatrooms/{targetUser}/messages")
+    public ResponseEntity<Stream<ChatRoom>> fetchAllMessages(@PathVariable String userName, @PathVariable String targetUser){
+        Collection<ChatRoom> chatRooms = chatService.getChatRooms(userName);
+        Stream<ChatRoom> messages = chatRooms.stream().filter(s -> s.getTarget().equals(targetUser));
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 }
