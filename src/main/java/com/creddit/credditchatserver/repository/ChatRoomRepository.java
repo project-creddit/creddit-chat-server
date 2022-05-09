7adb1f7ec3bdd2f32ad3fe7415b638a3a8dc0f97
oereo <dlstpgns0406@gmail.com>
2022-05-10 05:46:13 +0900

    feat: ChatRoomRepository 추가

--- src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
+++ src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepository.java
@@ -1,4 +1,12 @@
-package com.creddit.credditchatserver.repository;
-
-public interface ChatRoomRepository {
-}
+//package com.creddit.credditchatserver.repository;
+//
+//import com.creddit.credditchatserver.entity.ChatRoom;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//
+//public interface ChatRoomRepository {
+//    void save(ChatRoom chatRoom);
+//    void update(ChatRoom chatRoom);
+//}chatRoom
+//
--- src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepositoryImpl.java
+++ src/main/java/com/creddit/credditchatserver/repository/ChatRoomRepositoryImpl.java
@@ -1,2 +1,13 @@
-package com.creddit.credditchatserver.repository;public class ChatRoomRepositoryImpl {
-}
+//compackage com.creddit.credditchatserver.repository;
+//
+//import com.creddit.credditchatserver.entity.ChatRoom;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class ChatRoomRepositoryImpl implements ChatRoomRepository{
+//
+//    @Override
+//    public void update(ChatRoom chatRoom) {
+//        save(chatRoom);
+//    }
+//}
