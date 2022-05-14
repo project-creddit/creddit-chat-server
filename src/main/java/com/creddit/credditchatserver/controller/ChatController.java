1324b35474c6e245ee89153df9cafe59ca82f4d7
oereo <dlstpgns0406@gmail.com>
2022-05-15 07:42:48 +0900

    feat: fetchAllChatRooms api에 현재 유저가 leftusers field안에 있을 경우 filter로 제외

--- src/main/java/com/creddit/credditchatserver/controller/ChatController.java
+++ src/main/java/com/creddit/credditchatserver/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.creddit.credditchatserver.controller;
 
 import com.creddit.credditchatserver.dto.ChatRoomIdDto;
+import com.creddit.credditchatserver.dto.ChatRoomRegisterDto;
 import com.creddit.credditchatserver.entity.ChatRoom;
 import com.creddit.credditchatserver.entity.User;
 import com.creddit.credditchatserver.service.ChatService;
 import com.creddit.credditchatserver.trace.TraceAspect;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Import;
@@ -27,23 +29,23 @@ public class ChatController {
 
     private ChatService chatService;
 
-    @GetMapping("/register/{myId}/{userId}")
-    public ResponseEntity<?> registerChatRoom(@PathVariable String userId, @PathVariable String myId) throws Exception{
-        chatService.createChatRoom(userId, myId);
+    @PostMapping("/register")
+    public ResponseEntity<?> registerChatRoom(@RequestBody ChatRoomRegisterDto chatRoomRegisterDto) throws Exception{
+        chatService.createChatRoom(chatRoomRegisterDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{userName}/chatrooms")
-    public ResponseEntity<Stream<ChatRoom>> fetchAllChatRooms(@PathVariable String userName){
+    public ResponseEntity<Stream<ChatRoom>> fetchAllChatRooms(@PathVariable String userName) throws JsonProcessingException {
         Collection<ChatRoom> chatRooms = chatService.getChatRooms(userName);
-        Stream<ChatRoom> messages = chatRooms.stream().filter(s -> s.getUsers().contains(userName));
-//                .filter(s -> !s.getLeftUsers().contains(userName));
+        Stream<ChatRoom> messages = chatRooms.stream()
+                .filter(s -> !s.getLeftUsers().contains(userName));
 
         return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 
     @GetMapping("/{userName}/chatrooms/{targetUser}/messages")
-    public ResponseEntity<ChatRoom> fetchAllMessages(@PathVariable String userName, @PathVariable String targetUser){
+    public ResponseEntity<ChatRoom> fetchAllMessages(@PathVariable String userName, @PathVariable String targetUser) throws JsonProcessingException {
         Collection<ChatRoom> chatRooms = chatService.getChatRooms(userName);
         ChatRoom messages = chatRooms.stream().filter(s -> s.getTarget().equals(targetUser)).findFirst().get();
         return new ResponseEntity<>(messages, HttpStatus.OK);
