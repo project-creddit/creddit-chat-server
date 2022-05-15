c4bc6bde5cf0589c4e5d13e4f654b658193e9279
oereo <dlstpgns0406@gmail.com>
2022-05-15 17:41:56 +0900

    feat: MessageEntity에 Setter annotation 추가

--- src/main/java/com/creddit/credditchatserver/entity/Message.java
+++ src/main/java/com/creddit/credditchatserver/entity/Message.java
@@ -3,11 +3,13 @@ package com.creddit.credditchatserver.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class Message {
 
     private String sender;
